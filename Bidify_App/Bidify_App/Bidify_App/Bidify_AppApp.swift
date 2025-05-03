cac590928a037e9aa91a364bcb184ca11ef94037
HasaraDissanayake <dmhasara.d@gmail.com>
2025-05-03 18:49:11 +0800

    Add files via upload
    Initial Commit

---
+++ Bidify_App/Bidify_App/Bidify_App/Bidify_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bidify_AppApp.swift
+//  Bidify_App
+//
+//  Created by user271456 on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Bidify_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bidify_App/Bidify_App/Bidify_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bidify_App
+//
+//  Created by user271456 on 5/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
